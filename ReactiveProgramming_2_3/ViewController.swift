638dd9f34e674b02009d69cd0befb9cdbaacd59e
laurayesayan <laurayesayan@gmail.com>
2020-02-14 12:39:25 +0300

    Initial commit. Email and password checking are working well.

--- ReactiveProgramming_2_3/ViewController.swift
+++ ReactiveProgramming_2_3/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import Bond
+import ReactiveKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        infoLabel.isUserInteractionEnabled = true
+        emailTextField.reactive.text
+            .ignoreNils()
+            .filter{$0.count > 5}
+            .map{$0.isValidEmail ? "" : "Incorrect e-mail"}
+            .bind(to: infoLabel.reactive.text)
+        
+        passwordTextField.reactive.text
+            .ignoreNils()
+            .filter{$0.count != 0}
+            .map{$0.isValidPassword ? "" : "Too short password"}
+            .bind(to: infoLabel.reactive.text)
+        
+        combineLatest(emailTextField.reactive.text, passwordTextField.reactive.text) { email, password in
+            return email!.isValidEmail && password!.isValidPassword
+        }
+        .bind(to: sendButton.reactive.isEnabled)
+    
     }
+    
+}
 
-
+extension String {
+    var isValidEmail: Bool {
+        return NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}").evaluate(with: self)
+    }
+    var isValidPassword: Bool {
+        return self.count >= 6
+    }
 }
 
